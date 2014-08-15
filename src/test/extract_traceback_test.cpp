953aca971434c7fab63bd9425b3f23d828404753
Austin Bingham <austin.bingham@gmail.com>
2014-08-15 17:31:53 +0200

    Using the new execute_python_code_* functions.

--- src/test/extract_traceback_test.cpp
+++ src/test/extract_traceback_test.cpp
@@ -9,6 +9,7 @@
 
 #include <boost_python_exception/get_exception_info.hpp>
 
+#include "helpers/execute_python_code.hpp"
 #include "helpers/fixtures/clear_python_errors.hpp"
 
 namespace bp = boost::python;
@@ -16,35 +17,24 @@ namespace bpe = boost_python_exception;
 
 namespace {
 
-	void execute_python_code_in_main_module(std::string const & python_code)
-	{
-		bp::object module = bp::import("__main__");
-		bp::object dict = module.attr("__dict__");
-		try {
-			boost::python::exec(python_code.c_str(), dict);
-		} catch(bp::error_already_set const &){
-			// suppress error
-		}
-	}
+ void verify_traceback(std::string const & expected_traceback, bp::object py_traceback)
+ {
+  bpe::traceback tb = bpe::extract_traceback(py_traceback);
 
-	void verify_traceback(std::string const & expected_traceback, bp::object py_traceback)
-	{
-		bpe::traceback tb = bpe::extract_traceback(py_traceback);
+  std::ostringstream actual;
+  for (bpe::traceback::const_iterator it = tb.begin(); it != tb.end(); ++it) {
+   actual << *it << "\n";
+  }
 
-		std::ostringstream actual;
-		for (bpe::traceback::const_iterator it = tb.begin(); it != tb.end(); ++it) {
-			actual << *it << "\n";
-		}
+  BOOST_CHECK_EQUAL(expected_traceback, actual.str());
+ }
 
-		BOOST_CHECK_EQUAL(expected_traceback, actual.str());
-	}
-
-	void test_traceback(std::string const & python_code, std::string const & expected_traceback)
-	{
-		execute_python_code_in_main_module(python_code);
-		bpe::exception_info const info = bpe::get_exception_info();
-		verify_traceback(expected_traceback, info.traceback);
-	}
+ void test_traceback(std::string const & python_code, std::string const & expected_traceback)
+ {
+  test::execute_python_code_in_main_module(python_code, true);
+  bpe::exception_info const info = bpe::get_exception_info();
+  verify_traceback(expected_traceback, info.traceback);
+ }
 
 }
 
@@ -52,35 +42,35 @@ BOOST_FIXTURE_TEST_SUITE(extract_traceback, test::fixtures::clear_python_errors)
 
 BOOST_AUTO_TEST_CASE(no_error)
 {
-	std::string const python_code("1 + 2");
-	std::string const expected_traceback("");
+ std::string const python_code("1 + 2");
+ std::string const expected_traceback("");
 
-	test_traceback(python_code, expected_traceback);
+ test_traceback(python_code, expected_traceback);
 }
 
 BOOST_AUTO_TEST_CASE(single_line_with_error)
 {
-	std::string const python_code("1 + \"2\"");
-	std::string const expected_traceback("File \"<string>\", line 1, in <module>\n");
+ std::string const python_code("1 + \"2\"");
+ std::string const expected_traceback("File \"<string>\", line 1, in <module>\n");
 
-	test_traceback(python_code, expected_traceback);
+ test_traceback(python_code, expected_traceback);
 }
 
 BOOST_AUTO_TEST_CASE(function_call_with_error)
 {
-	std::string const python_code(
-			"def main():\n"
-			"\t 1 + \"2\"\n"
-			"if __name__ == \"__main__\":\n"
-			"\t main()\n"
-		);
+ std::string const python_code(
+   "def main():\n"
+   "\t 1 + \"2\"\n"
+   "if __name__ == \"__main__\":\n"
+   "\t main()\n"
+  );
 
-	std::string const expected_traceback(
-			"File \"<string>\", line 4, in <module>\n"
-			"File \"<string>\", line 2, in main\n"
-		);
+ std::string const expected_traceback(
+   "File \"<string>\", line 4, in <module>\n"
+   "File \"<string>\", line 2, in main\n"
+  );
 
-	test_traceback(python_code, expected_traceback);
+ test_traceback(python_code, expected_traceback);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
