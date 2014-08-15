20b5546b25b585b84b085c5c9d5ad34e3e7beab6
Michael König <michael.koenig@blue-yonder.com>
2014-08-15 13:21:31 +0200

    Fixed bad merges I had because Eclipse's merge editor sucks

--- src/boost_python_exception/format_exception.hpp
+++ src/boost_python_exception/format_exception.hpp
@@ -7,18 +7,10 @@ namespace boost_python_exception {
 
 class exception_info;
 
-<<<<<<< HEAD
 /* Generate a nicely formatted string for the given exception_info.
  * This is well-behaved when the values are ``None``.
  */
 std::string format_exception(exception_info const & ex_info);
-=======
-/* Generate a nicely formatted string for the (type, value, traceback)
- * tuple ``ex_info``. This is well-behaved when the values are
- * ``None``.
- */
-std::string format_exception(const exception_info& ex_info);
->>>>>>> upstream/master
 
 /* Calls ``format_exception()`` with the return value of
  * ``getExceptionInfo()``.
